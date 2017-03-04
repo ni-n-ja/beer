6a1290adf8665080e706950e28c0026142aff77e
ni_n_ja <rampage4extrm@gmail.com>
2017-03-04 19:26:31 +0900

    a

--- js/web.js
+++ js/web.js
@@ -12,6 +12,17 @@ var alp;
 var bet;
 var gam;
 
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext;
+var buffer;
+var source;
+var request;
+
+var flag = 0;
+var timter;
+
+var beer;
+
 function handleOrientation(event) {
     absolute = event.absolute;
     alpha = event.alpha;
@@ -20,7 +31,7 @@ function handleOrientation(event) {
 }
 
 function animate(timestamp) {
-    context.clearRect(0, 0, 200, 200);
+    context.clearRect(0, 0, 200, 360);
 
     context.fillStyle = 'rgba(255,0,135,64)';
     context.fillRect(0, 0, 50, absolute);
@@ -39,11 +50,32 @@ function animate(timestamp) {
     bet.innerHTML = "beta: " + beta;
     gam.innerHTML = "gumma: " + gamma;
 
+    if (flag === 0 && beta < 0) {
+        beer.add("deg");
+        timer = window.setInterval(function() {
+            window.navigator.vibrate(100);
+        }, 200);
+
+        flag = 1;
+        source = audioContext.createBufferSource();
+        audioContext.decodeAudioData(buffer, function(buf) {
+            source.buffer = buf;
+            source.connect(audioContext.destination);
+            source.start(0);
+        });
+    }
+    if (flag === 1 && beta > 1) {
+        beer.remove("deg");
+        clearInterval(timer);
+        source.stop(0);
+        flag = 0;
+    }
+
     requestAnimationFrame(animate);
 }
 
 window.onload = function() {
-
+    beer = document.getElementById("beer").classList;
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
 
@@ -54,6 +86,37 @@ window.onload = function() {
 
     window.addEventListener("deviceorientation", handleOrientation, true);
     console.log(absolute, alpha, beta, gamma);
-    animate();
 
+    audioContext = new AudioContext();
+    buffer = null;
+    source = audioContext.createBufferSource();
+    request = new XMLHttpRequest();
+    request.open('GET', 's2.mp3', true);
+    request.responseType = 'arraybuffer';
+    request.send();
+    request.onload = function() {
+        buffer = request.response;
+        animate();
+    };
+
+    document.body.onclick = function() {
+        if (flag === 0) {
+            timer = window.setInterval(function() {
+                window.navigator.vibrate(100);
+            }, 200);
+            beer.add("deg");
+            flag = 1;
+            source = audioContext.createBufferSource();
+            audioContext.decodeAudioData(buffer, function(buf) {
+                source.buffer = buf;
+                source.connect(audioContext.destination);
+                source.start(0);
+            });
+        } else if (flag === 1) {
+            beer.remove("deg");
+            clearInterval(timer);
+            source.stop(0);
+            flag = 0;
+        }
+    };
 }
