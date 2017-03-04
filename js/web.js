11216caefbb22411a1cf8a8f2cf893f6c4bd0de8
ni_n_ja <rampage4extrm@gmail.com>
2017-03-04 16:11:15 +0900

    a

--- js/web.js
+++ js/web.js
@@ -6,41 +6,51 @@ var absolute = 0;
 var alpha = 0;
 var beta = 0;
 var gamma = 0;
+
+
+function handleOrientation(event) {
+    absolute = event.absolute;
+    alpha = event.alpha;
+    beta = event.beta;
+    gamma = event.gamma;
+}
+
+function animate(timestamp) {
+    context.clearRect(0, 0, 200, 200);
+
+    context.strokeStyle = 'rgba(255,0,135,64)';
+    context.beginPath();
+    context.moveTo(1, 0);
+    context.lineTo(1, absolute);
+    context.stroke();
+
+    context.strokeStyle = 'rgba(255,0,135,64)';
+    context.beginPath();
+    context.moveTo(3, 0);
+    context.lineTo(3, alpha);
+    context.stroke();
+
+    context.strokeStyle = 'rgba(255,0,135,64)';
+    context.beginPath();
+    context.moveTo(4, 0);
+    context.lineTo(4, beta);
+    context.stroke();
+
+    context.strokeStyle = 'rgba(255,0,135,64)';
+    context.beginPath();
+    context.moveTo(6, 0);
+    context.lineTo(6, gamma);
+    context.stroke();
+
+    requestAnimationFrame(animate);
+}
+
 window.onload = function() {
 
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
     window.addEventListener("deviceorientation", handleOrientation, true);
     console.log(absolute, alpha, beta, gamma);
+    animate();
 
-    function animate(timestamp) {
-        context.clearRect(0, 0, 600, 480);
-
-        context.strokeStyle = 'rgba(255,0,135,64)';
-        context.beginPath();
-        context.moveTo(1, 0);
-        context.lineTo(1, absolute);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(3, 0);
-        context.lineTo(3, alpha);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(4, 0);
-        context.lineTo(4, beta);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(6, 0);
-        context.lineTo(6, gamma);
-        context.stroke();
-
-        requestAnimationFrame(animate);
-    }
-
-    function handleOrientation(event) {
-        absolute = event.absolute;
-        alpha = event.alpha;
-        beta = event.beta;
-        gamma = event.gamma;
-    }
 }
